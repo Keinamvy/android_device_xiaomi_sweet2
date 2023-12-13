aaea62a94e6dacd8a41309f845ecb8649e9561d6
Alexander Winkowski <dereference23@outlook.com>
2023-12-13 14:04:59 +0000

    sweet: Redmi Note 10 Pro -> Redmi Note 12 Pro
    Change-Id: I1ea3fd8559ff9388abdf250526a6f786c1ed2d8f

--- lineage_sweet2.mk
+++ lineage_sweet2.mk
@@ -10,19 +10,19 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from sweet device
-$(call inherit-product, device/xiaomi/sweet/device.mk)
+$(call inherit-product, device/xiaomi/sweet2/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
-PRODUCT_DEVICE := sweet
+PRODUCT_NAME := lineage_sweet2
+PRODUCT_DEVICE := sweet2
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi Note 10 Pro
+PRODUCT_MODEL := Redmi Note 12 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys" \
-    BuildFingerprint=Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
+    BuildDesc="sweet_global2-user 13 TKQ1.221114.001 V816.0.13.0.THGMIXM release-keys" \
+    BuildFingerprint=Redmi/sweet_global2/sweet:13/TKQ1.221114.001/V816.0.13.0.THGMIXM:user/release-keys
