ab66e13dbcc301dc84a46f087898fb8d94e1b7d5
Keinamvy <nguyennhatminhlol90@gmail.com>
2026-04-29 14:51:44 +0000

    sweet2: Drop common LiveDisplay service
    The common tree automatically includes the xiaomi_sm6150 LiveDisplay
    service via PRODUCT_PACKAGES. Since we are using a device-specific
    implementation (or want to avoid a conflict), use filter-out to
    remove it from the build without needing to modify the common
    source tree.
    
    Signed-off-by: Keinamvy <nguyennhatminhlol90@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
+# Remove the livedisplay service inherited from the common tree
+PRODUCT_PACKAGES := $(filter-out vendor.lineage.livedisplay-service.xiaomi_sm6150, $(PRODUCT_PACKAGES))
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
